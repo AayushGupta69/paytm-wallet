8407eb91ac842d202b0c6fbfd14badc2390930da
Aayush Gupta <aayushgupta69official@gmail.com>
2024-10-03 21:12:59 +0530

    Updated page.tsx in merchant-app

--- apps/merchant-app/app/page.tsx
+++ apps/merchant-app/app/page.tsx
@@ -1,10 +1,8 @@
-export default function Home() {
-  return (
-    <div className="font-bold">
-      <div className="bg-purple-500">Merchant App</div>
-      <div className="bg-red-500">Red</div>
-      <div className="bg-green-500">Green</div>
-      <div className="bg-blue-500">Blue</div>
-    </div>
-  );
+"use client";
+
+import { useBalance } from "@paytm-wallet/store/balance";
+
+export default function () {
+  const balance = useBalance();
+  return <div>hi there {balance}</div>;
 }
