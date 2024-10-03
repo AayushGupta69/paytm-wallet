fa21340978130d493a8a5857e8dad34662eecd5e
Aayush Gupta <72346589+AayushGupta69@users.noreply.github.com>
2024-10-03 07:07:05 +0530

    Added tailwind to the ui package, updated button component and appbar component. (#1)
    * Renamed namespace in ui package
    
    * Added tailwind to ui package
    
    * Initialized tailwindcss and updated button component
    
    * Fixed issues with ui package and added a new appbar component
    
    * Imported css in merchant app for ui package

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -4,15 +4,15 @@ import { ReactNode } from "react";
 
 interface ButtonProps {
   children: ReactNode;
-  className?: string;
-  appName: string;
+  onClick: () => void;
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+export const Button = ({ onClick, children }: ButtonProps) => {
   return (
     <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
+      onClick={onClick}
+      type="button"
+      className="ui-text-white ui-bg-gray-800 hover:ui-bg-gray-900 focus:ui-outline-none focus:ui-ring-4 focus:ui-ring-gray-300 ui-font-medium ui-rounded-lg ui-text-sm ui-px-5 ui-py-2.5 ui-me-2 ui-mb-2"
     >
       {children}
     </button>
