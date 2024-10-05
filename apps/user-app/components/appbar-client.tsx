267fc7955cdf16585374bff62214420ad57b5332
Aayush Gupta <aayushgupta69official@gmail.com>
2024-10-06 01:38:22 +0530

    Added a new appbar client component in user-app

---
+++ apps/user-app/components/appbar-client.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Appbar } from "@paytm-wallet/ui/appbar";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+
+export default function AppbarClient() {
+  const session = useSession();
+  const router = useRouter();
+
+  return (
+    <div>
+      <Appbar
+        onSignin={signIn}
+        onSignout={async () => {
+          await signOut();
+          router.push("/api/auth/signin");
+        }}
+        user={session.data?.user}
+      />
+    </div>
+  );
+}
