ebe29fefe33c8903b5a5a54998844d5deee5a7d2
Aayush Gupta <aayushgupta69official@gmail.com>
2024-10-12 23:39:56 +0530

    Updated root layout.tsx to add appbar client

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import "@paytm-wallet/ui/styles.css";
 import { Providers } from "../providers";
+import AppbarClient from "../components/appbar-client";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,6 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <Providers>
         <body className={`${geistSans.variable} ${geistMono.variable}`}>
+          <AppbarClient />
           {children}
         </body>
       </Providers>
