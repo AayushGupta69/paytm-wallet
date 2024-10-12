7a8d7429b52978835a6902941cb3fc750b212aaf
Aayush Gupta <aayushgupta69official@gmail.com>
2024-10-12 23:27:20 +0530

    Added recoil and next-auth along with providers and updated root layout.tsx

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import "@paytm-wallet/ui/styles.css";
+import { Providers } from "../providers";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,9 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+      <Providers>
+        <body className={`${geistSans.variable} ${geistMono.variable}`}>
+          {children}
+        </body>
+      </Providers>
     </html>
   );
 }
