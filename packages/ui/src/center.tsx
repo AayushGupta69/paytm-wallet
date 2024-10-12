6de59236645ed80860a7f654b57e7c18d7ee7a6a
Aayush Gupta <aayushgupta69official@gmail.com>
2024-10-12 23:19:56 +0530

    Added a new center component in ui

---
+++ packages/ui/src/center.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Center = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="ui-flex ui-justify-center ui-flex-col ui-h-full">
+      <div className="ui-flex ui-justify-center">{children}</div>
+    </div>
+  );
+};
